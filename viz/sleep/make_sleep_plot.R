1557acecaf5496dca323011b67411d5cc2dc97ff
Matt Kaye <mrkaye97@gmail.com>
2020-06-11 08:28:15 -0400

    update sleep plot making, add year label below months

--- viz/sleep/make_sleep_plot.R
+++ viz/sleep/make_sleep_plot.R
@@ -16,7 +16,7 @@ df <- tbl(con, 'sleep') %>%
   arrange(date) %>%
   filter(mainSleep == 1) %>% # main sleep = no naps
   collect() %>%
-  mutate(date = as_datetime(date),
+  mutate(date = as_date(date),
          startTime = as_datetime(startTime),
          sh = ifelse(hour(startTime) < 8, hour(startTime) + 24, hour(startTime)), #create numeric times
          sm = minute(startTime),
@@ -25,13 +25,14 @@ df <- tbl(con, 'sleep') %>%
          em = minute(endTime),
          et = eh + em/60,
          mst = ma(st, 7), #create moving averages
-         met = ma(et, 7)
+         met = ma(et, 7),
+         year = year(startTime)
   )
 
 plt <- df %>%
     ggplot(aes(x = date))+
-    geom_line(aes(y = et), color = 'coral', alpha = .3)+
-    geom_line(aes(y = st), color = 'dodgerblue', alpha = .3)+
+    geom_line(aes(y = et), color = 'coral', alpha = .3, na.rm = T)+
+    geom_line(aes(y = st), color = 'dodgerblue', alpha = .3, na.rm = T)+
     geom_line(aes(y = met), color = 'coral', na.rm=T)+
     geom_line(aes(y = mst), color = 'dodgerblue', na.rm=T)+
     scale_y_continuous(breaks = seq(0, 30, 2),
@@ -40,6 +41,10 @@ plt <- df %>%
     )+
     labs(x = "Date",
          y = 'Time')+
-    theme_fivethirtyeight()
+    theme_fivethirtyeight()+
+  scale_x_date(date_breaks = '1 month', date_labels = '%b', expand = c(0, 0))+
+  facet_grid(. ~ year, space = 'free', scales = 'free_x', switch = 'x') +
+  theme(panel.spacing.x = unit(0,"line"), 
+        strip.placement = "outside")
 
 ggsave('sleepplot.svg', plt, 'svg', dpi = 'retina', path = '~/Documents/GitHub/fitbit/viz', width = 6, height = 4.8)
